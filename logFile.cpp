4893807a8e0b73fa30e44f0bac1c47b6e3a5630e
Melany Meichtri <62313691+mmeichtri@users.noreply.github.com>
2021-02-21 13:50:47 -0300

    Update logFile.cpp
    .

--- logFile.cpp
+++ logFile.cpp
@@ -8,13 +8,11 @@ using namespace std;
 LogFile::LogFile(){
 }
 
-void LogFile::crearLog(){
+void LogFile::actualizarLog(Personaje* personaje){
     fstream file;
     file.open("partida.csv");
-
+    
+    file << personaje->getElemento() +","<< personaje->getNombre() +","<< personaje->getEscudo() +","<< pesonaje->getVida() +","<< personaje->mostrarEnergiaActual() +","<< personaje->getFila() +","<< persoanje->getColumna() << endl;
+    
     file.close();
-}
-
-void LogFile::actualizarLog(Personaje* personaje){
-    this->file << personaje->getElemento() +","<< personaje->getNombre() +","<< personaje->getEscudo() +","<< pesonaje->getVida() +","<< personaje->mostrarEnergiaActual() +","<< personaje->getFila() +","<< persoanje->getColumna() << endl;
 }
