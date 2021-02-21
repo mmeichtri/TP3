ef4933be4e52d66525f02e4aa705b3c4ccce7f7d
Melany Meichtri <62313691+mmeichtri@users.noreply.github.com>
2021-02-21 13:52:05 -0300

    Update logFile.h
    .

--- logFile.h
+++ logFile.h
@@ -15,12 +15,6 @@ public:
 
     /*
     pre:
-    post: crea el archivo a actualizarse en cada movimiento que realice cada jugador de sus personajes
-    */
-    void crearLog();
-
-    /*
-    pre:
     post: actualiza el log, agregando un nuevo movimento
     */
     void actualizarLog(Personaje* );
